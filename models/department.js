ea00546234aaef55cd536e535cbfb5f34bfc566a
Sylvain David <youremail@gmail.com>
2020-02-05 21:01:41 -0500

    3.0

---
+++ models/department.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+    var Department = sequelize.define("Department", {
+        name: DataTypes.String
+    });
+  
+    Department.associate = function(models) {
+      Department.hasMany(models.Employee, {
+            onDelete: "cascade"
+        });
+    };
+  
+    return Department;
+  };
+  
